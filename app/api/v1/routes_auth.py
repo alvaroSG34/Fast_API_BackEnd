1f84fafd36f57988c66e8fc4d453f28fcb7c32b0
Alvaro Sonco <164678960+alvaroSG34@users.noreply.github.com>
2025-04-10 18:49:49 -0400

    Login routes
    done

--- app/api/v1/routes_auth.py
+++ app/api/v1/routes_auth.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.db.session import SessionLocal
 from app.models.user import User
 from app.schemas.user import UserLogin, Token
+from fastapi.security import OAuth2PasswordRequestForm
 from app.services.auth import verify_password, create_access_token
 
 router = APIRouter()
@@ -15,10 +16,10 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/login", response_model=Token, status_code=200)
-def login(user: UserLogin, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.email == user.email).first()
-    if not db_user or not verify_password(user.password, db_user.hashed_password):
+@router.post("/login", response_model=Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == form_data.username).first()
+    if not db_user or not verify_password(form_data.password, db_user.hashed_password):
         raise HTTPException(status_code=401, detail="Credenciales incorrectas")
 
     token = create_access_token(data={"sub": str(db_user.id)})
